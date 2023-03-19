00f08477339925599bcb80937cce15b026ad02c2
DELL <aycholpon25t@gmail.com>
2023-03-19 17:32:42 +0600

    develop

--- app/src/main/java/com/example/ibolot/ui/fragments/service/ServiceFragment.kt
+++ app/src/main/java/com/example/ibolot/ui/fragments/service/ServiceFragment.kt
@@ -1,41 +1,17 @@
 package com.example.ibolot.ui.fragments.service
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.example.ibolot.databinding.FragmentServiceBinding
 
 class ServiceFragment : Fragment() {
 
-    private var _binding: FragmentServiceBinding? = null
+    private lateinit var binding: FragmentServiceBinding
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(ServiceViewModel::class.java)
-
-        _binding = FragmentServiceBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-       // val textView: TextView = binding.textDashboard
-       // dashboardViewModel.text.observe(viewLifecycleOwner) {
-        //    textView.text = it
-        //}
-        return root
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentServiceBinding.inflate(layoutInflater)
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
 }
