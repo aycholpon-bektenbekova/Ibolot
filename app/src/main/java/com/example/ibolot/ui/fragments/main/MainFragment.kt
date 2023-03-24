8c5485c3dda95564d0351f26adc44e4564db38f8
ukuevbeksultan <ukuevb08@gmail.com>
2023-03-25 02:31:46 +0600

    Полностью закончили верстку! Осталось Бекенд и связка!

--- app/src/main/java/com/example/ibolot/ui/fragments/main/MainFragment.kt
+++ app/src/main/java/com/example/ibolot/ui/fragments/main/MainFragment.kt
@@ -5,6 +5,8 @@ import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.ibolot.R
 import com.example.ibolot.databinding.FragmentMainBinding
@@ -32,12 +34,27 @@ class MainFragment : BaseFragment(R.layout.fragment_main) {
         binding.viewPager2.adapter = adapter
         binding.dotsInd.setViewPager2(binding.viewPager2)
 
-        Log.e("ololo" , "ololo")
-        imageList.add(PagerItem(R.drawable.main_img.toString() , 1))
-        imageList.add(PagerItem(R.drawable.main_img.toString() , 2))
-        imageList.add(PagerItem(R.drawable.main_img.toString() , 3))
-        imageList.add(PagerItem(R.drawable.main_img.toString() , 4))
+        Log.e("ololo", "ololo")
+        imageList.add(PagerItem(R.drawable.main_img.toString(), 1))
+        imageList.add(PagerItem(R.drawable.main_img.toString(), 2))
+        imageList.add(PagerItem(R.drawable.main_img.toString(), 3))
+        imageList.add(PagerItem(R.drawable.main_img.toString(), 4))
     }
+
     private fun onItemClick(item: PagerItem) {
+
+        binding.cardDoctors.setOnClickListener {
+            findNavController().navigate(R.id.doctorsFragment)
+        }
+        binding.cardChildren.setOnClickListener {
+            Toast.makeText(context, "Раздел находится в разработке", Toast.LENGTH_SHORT).show()
+        }
+        binding.cardAbout.setOnClickListener {
+            Toast.makeText(context, "Раздел находится в разработке", Toast.LENGTH_SHORT).show()
+        }
+        binding.cardServices.setOnClickListener {
+            findNavController().navigate(R.id.navigation_service)
+        }
+
     }
 }
